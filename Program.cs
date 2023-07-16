c9a7f8cb53aa5ac5d7893b0de68fe910e84524a3
mrunal238 <132991825+mrunal238@users.noreply.github.com>
2023-07-16 13:55:05 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
-﻿using System;
-namespace TemparatureConversion
+﻿namespace Rectangle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            TemperatureConverter temp = new TemperatureConverter();
-            Console.Write("give celsius temparature for conversion :");
-            string val1 = Console.ReadLine();
-            double fahrenheit = temp.CelsiusToFahrenheit(val1);
-            Console.WriteLine($"the value of {val1} celsius is {fahrenheit}fahrenheit");
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sumOfDigits = CalculateSumOfDigits(number);
+
+            if (number % sumOfDigits == 0)
+            {
+                Console.WriteLine("The number is divisible by the sum of its digits.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not divisible by the sum of its digits.");
+            }
+
+            DrawRectangle(number);
+        }
+
+        static int CalculateSumOfDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+
+        static void DrawRectangle(int digit)
+        {
+            for (int row = 1; row <= 5; row++)
+            {
+                for (int col = 1; col <= 3; col++)
+                {
+                    if (row == 1 || row == 5 || col == 1 || col == 3)
+                    {
+                        Console.Write(digit);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
