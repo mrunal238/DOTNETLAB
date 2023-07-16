50042013a14ba577d56ac58210d021991be3c924
mrunal238 <132991825+mrunal238@users.noreply.github.com>
2023-07-16 13:53:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TemparatureConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConverter temp = new TemperatureConverter();
+            Console.Write("give celsius temparature for conversion :");
+            string val1 = Console.ReadLine();
+            double fahrenheit = temp.CelsiusToFahrenheit(val1);
+            Console.WriteLine($"the value of {val1} celsius is {fahrenheit}fahrenheit");
+        }
+    }
+}
